d961dbff2cfecc78a0f3be1ff41a3d41c6441519
林志军 <linzhijun@xmpaoyou.com>
2024-02-28 10:34:54 +0800

    修改router模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/IndexView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import IndexView from '../views/IndexView.vue'
 
+const baseUrl = import.meta.env.BASE_URL.replace('.', '')
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(baseUrl),
   routes: [
     {
       path: '/',
       name: 'index',
-      component: HomeView
+      component: IndexView
     },
   ]
 })
+
 
 export default router
